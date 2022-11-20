aa82cbb46ccc61223e18d6d5008a45b1295f58b4
ylm78 <104013654+ylm78@users.noreply.github.com>
2022-11-20 23:18:52 +0100

    Update nom.h

--- nom.h
+++ nom.h
@@ -10,44 +10,40 @@
 #include <stdlib.h>
 #endif
 
-ttypedef struct adj_flechies{
+typedef struct nom_flechies{
     int nb_forme;
     char* forme_flechie;
-    char genre[3];      //Rien masculin feminin
+    char genre[3];      //masculin feminin
     char nombre_gram[2];    //singulier pluriel
-}adj_flechies;
+}nom_flechies;
 
-typedef struct cell_adj_flechies{
-    adj_flechies flechies;
-    struct cell_adj_flechies* next;
-}*p_flechie_adj;
+typedef struct cell_nom_flechies{
+    nom_flechies flechies;
+    struct cell_nom_flechies* next;
+}*p_flechie_nom;
 
-typedef struct liste_flechie_adj{
-    p_flechie_adj head;
-}liste_flechie_adj;
+typedef struct liste_flechie_nom{
+    p_flechie_nom head;
+}liste_flechie_nom;
 
-typedef struct node_adj{
-    char lettres;
+typedef struct node_nom{
+    char lettre;
     int nb_formes;
-    liste_flechie_adj l_flechie;
-    struct node_adj* sons[ALPHABET_SIZE];
-}node_adj;
+    liste_flechie_nom l_flechie;
+    struct node_nom* sons[ALPHABET_SIZE];
+}s_node_nom, *pnode_nom;
 
-typedef struct tree_adj{
-    node_adj root;
-}s_tree_adj;
+typedef struct tree_nom{
+    pnode_nom root;
+}s_tree_nom;
 
-void copy_adj(char *arrive,char*depart);
-int IsAdjInFichier(char*);
-void ajt_adj_txt(char*);
-node_adj *new_node_adj(char);
-s_tree_adj tree_adj();
-
-
-//protypes
+//Prototype fonction
+void display_nom(char*);
+void copy_nom(char *,char*);
+s_node_nom *new_node_nom(char );
+s_tree_nom tree_nom();
+int IsNomInFichier(char*);
 void copy_nom(char *arrive,char*depart);
-s_node *Create_node(char val);
-int lettre_existante(s_node * noeud, char lettre);
-char supp_lettre(char *chaine);
-void ajouter_mot(s_node * noeud,char* LeMot);
-s_tree Creer_arbre_nom();
+void ajt_nom_txt(char*);
+#endif //RANDOM_TENSE_NOM_H
+
